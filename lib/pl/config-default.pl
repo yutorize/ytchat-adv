ea61ef99b178cce736835b8da5a9f724532c401b
ViVi <vivi.shark.tech+github@gmail.com>
2024-02-13 10:57:12 +0900

    定義済みランダム表に、SW2.5のパニッシュ／フィアー表を追加
    ダイスロール式のランダム表のサンプルがなかったので

--- lib/pl/config-default.pl
+++ lib/pl/config-default.pl
@@ -57,6 +57,7 @@ our %random_table = (
   'トランプ' => { 'data'=>'trump.txt', 'help'=>'全53枚のトランプのカードです' },
   'トランプ2' => { 'data'=>'trump-wjoker.txt', 'help'=>'全54枚のトランプのカードです（ジョーカー2枚）' },
   '大アルカナ' => { 'data'=>'major-arcana.txt', 'faces' => ['正位置','逆位置'], 'help'=>'全21枚のタロットカード（大アルカナ）です' },
+  'パニッシュ/フィアー表' => { 'data'=>'sw2-punish-fear.txt', 'help'=>'SW2.5のパニッシュ／フィアー表です' },
 );
 our $rtable_dir = './rtable/';
 
